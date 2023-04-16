0bfffc05b22df9093822fcc29d0f4e121117cf7b
Zadie Moon <98183566+zprintln@users.noreply.github.com>
2023-04-16 16:53:23 -0700

    📝Implement Message Generator Methods

--- src/Model.java
+++ src/Model.java
@@ -1,10 +1,29 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Model implements IModel{
   //declare an array to store answer choices in
   private ArrayList<Character> scores = new ArrayList<>();
   //declare a variable to represent the final test score
   private char answer;
+  private ArrayList<String> msgs = new ArrayList<>();
+
+  /**
+   * This is a constructor of a model object
+   */
+  public Model(){
+    msgs.add("Be yourself like Hello Kitty and let your uniqueness shine!");
+    msgs.add("Don't let bad days get you down, be as resilient as My Melody!");
+    msgs.add("Spread love and kindness like Cinnamoroll's fluffy clouds!");
+    msgs.add("Like Little Twin Stars, share your dreams with those you trust.");
+    msgs.add(" Stay positive and keep smiling like Pompompurin!");
+    msgs.add("Embrace your creativity like Keroppi and explore new things.");
+    msgs.add("Be loyal and caring like Pochacco, and cherish your friendships.");
+    msgs.add("Live life to the fullest like Kuromi, and don't be afraid to take risks.");
+    msgs.add(" Have a curious spirit like Badtz-Maru and always ask questions.");
+    msgs.add(" Keep a positive attitude like Tuxedo Sam, and face challenges with grace.");
+
+  }
 
   /**
    * This method generates a random positive message from a list of messages
@@ -12,7 +31,16 @@ public class Model implements IModel{
    */
   @Override
   public String randomMsgGenerator(){
-    return "Test string";
+    /*
+    //generate a random number from 1-10
+    Random rand = new Random();
+    int num = rand.nextInt(10) + 1;
+    //return the message in position rand num
+    return msgs.get(num);
+    */
+    Random rand = new Random();
+    int num = rand.nextInt(1);
+    return msgs.get(num);
   }
 
   /**
@@ -23,6 +51,11 @@ public class Model implements IModel{
   public String getPositiveMsg(){
     return randomMsgGenerator();
   }
+
+  /**
+   * This method returns a character that most relates to the player
+   * @return character
+   */
   @Override
   public String getCharacter(){
     return "Test character";
